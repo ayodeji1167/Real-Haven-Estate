8c2e77a19b913bec6bc174e7a24fecf9e91c8f4c
Ayodeji <afuwapeayodeji2018@gmail.com>
2022-07-20 15:59:39 +0100

    fixed branch issues

--- Real-Haven-Estate/src/error/custom-errors.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-useless-constructor */
-class CustomError extends Error {
-  constructor(message) {
-    super(message);
-  }
-}
-module.exports = CustomError;
--- Real-Haven-Estate/src/middlewares/not-found.js
+++
@@ -1,4 +0,0 @@
-const notFound = (req, res) => {
-  res.status(404).json({ message: 'Route Doesnt Exist' });
-};
-module.exports = notFound;
